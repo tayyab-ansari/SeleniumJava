09077704f8cba042089369af6f7a7073afe2f37a
tayyab-ansari <tayyabansari1505@gmail.com>
2024-07-01 16:10:41 +0500

    report fixed

--- src/test/java/qa/com/utils/ExtentReportManager.java
+++ src/test/java/qa/com/utils/ExtentReportManager.java
@@ -14,24 +14,24 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentReportManager implements ITestListener {
-	
+
 	public ExtentSparkReporter sparkReporter;
 	public ExtentReports extent;
 	public ExtentTest test;
-	
+
 	String repName;
-	
+
 	public void onStart(ITestContext testContext) {
-		
+
 		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
 		repName = "Test-Report-"+timeStamp+".html";
-		
-		sparkReporter = new ExtentSparkReporter(".\\reports\\"+repName);
-		
+
+		sparkReporter = new ExtentSparkReporter("./reports/"+repName);
+
 		sparkReporter.config().setDocumentTitle("RestAssuredFramework");
 		sparkReporter.config().setReportName("go rest API");
 		sparkReporter.config().setTheme(Theme.DARK);
-		
+
 		extent=new ExtentReports();
 		extent.attachReporter(sparkReporter);
 		extent.setSystemInfo("Application","WEB AUTOMATION");
@@ -39,17 +39,17 @@ public class ExtentReportManager implements ITestListener {
 		extent.setSystemInfo("Environemnt","QA");
 		extent.setSystemInfo("user","Umair");
 	}
-	
-	
+
+
 	public void onTestSuccess(ITestResult result)
 	{
 		test=extent.createTest(result.getName());
 		test.assignCategory(result.getMethod().getGroups());
 		test.createNode(result.getName());
 		test.log(Status.PASS, "Test Passed");
-				
+
 	}
-	
+
 	public void onTestFailure(ITestResult result)
 	{
 		test=extent.createTest(result.getName());
@@ -57,9 +57,9 @@ public class ExtentReportManager implements ITestListener {
 		test.assignCategory(result.getMethod().getGroups());
 		test.log(Status.FAIL, "TEST CASE FAILED IS ");
 		test.log(Status.FAIL, result.getThrowable().getMessage());
-	
+
 	}
-	
+
 	public void onTestSkipped(ITestResult result)
 	{
 		test=extent.createTest(result.getName());
@@ -69,10 +69,10 @@ public class ExtentReportManager implements ITestListener {
 		test.log(Status.SKIP, result.getThrowable().getMessage());
 
 	}
-	
+
 	public void onFinish(ITestContext testContext)
 	{
 		extent.flush();
 	}
-	
+
 }
